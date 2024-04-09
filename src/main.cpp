7f1c6302b5291bae798128070837ca7af3ea5c0a
jsecheverrir <145038266+jsecheverrir@users.noreply.github.com>
2024-04-08 21:46:55 -0500

    final main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,29 @@
 #include "funciones.h"
 #include <iostream>
 
-
 using namespace std;
 
-
-/*
 int main() {
     int cantidadMatrices;
-    cout << "Ingrese la cantidad de matrices en la cerradura: ";
-    cin >> cantidadMatrices;
+    string entradaCondiciones;
+
+
+    // Solicitar la fila y columna para encontrar la celda correspondiente
+    int fila, columna;
+    cout << "Ingrese la fila y columna de la celda a buscar: ";
+    cin >> fila >> columna;
+    int dimensionMinima = calcularDimensionMinima(fila,columna);
+
+
+    cout << "Ingrese las condiciones de la clave (en el formato 'condicion1 condicion2 ...', donde 1 es mayor, -1 es menor, y 0 es igual): ";
+    cin.ignore(); // Limpiar el buffer del teclado antes de leer la nueva entrada
+    getline(cin, entradaCondiciones);
+
+    cantidadMatrices = entradaCondiciones.length() + 1; //aqui debo poner algo para clave -1
 
     int* dimensiones = new int[cantidadMatrices]; // Arreglo para almacenar las dimensiones de cada matriz
-
-    // Pedir al usuario las dimensiones de cada matriz
     for (int i = 0; i < cantidadMatrices; ++i) {
-        cout << "Ingrese la dimension de la matriz " << (i + 1) << ": ";
-        cin >> dimensiones[i];
+        dimensiones[i] = dimensionMinima;
     }
 
     // Crear la cerradura de matrices
@@ -29,15 +36,9 @@ int main() {
         cout << endl;
     }
 
-    // Solicitar la fila y columna para encontrar la celda correspondiente
-    int fila, columna;
-    cout << "Ingrese la fila y columna de la celda a buscar: ";
-    cin >> fila >> columna;
-    //fila=fila-1;
-    //columna=columna-1;
 
     // Encontrar la celda correspondiente en cada matriz de la cerradura
-    encontrarCeldaCorrespondiente(cerradura, cantidadMatrices, dimensiones, fila, columna);
+    encontrarCeldaCorrespondiente(cerradura,cantidadMatrices, dimensiones, fila, columna);
 
     // Liberar la memoria utilizada por la cerradura y las dimensiones
     for (int i = 0; i < cantidadMatrices; ++i) {
@@ -48,4 +49,3 @@ int main() {
 
     return 0;
 }
-*/
